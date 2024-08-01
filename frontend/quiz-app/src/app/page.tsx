e8f0ff57595690de0791f156daafc068596e6a84
Vinicius Tabosa <viniciustabosa.dev@gmail.com>
2024-08-01 20:24:11 -0300

    add esqueleto

--- frontend/quiz-app/src/app/page.tsx
+++ frontend/quiz-app/src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'; // Ajuste o caminho conforme necessário
 import styles from './page.module.css'; // Se necessário, ajuste ou remova esta importação
-import Character from '@/components/characters';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Escolha Seu Personagem</h1>
-      <Character />
+      <BrowserRouter>
+      </BrowserRouter>
     </main>
   );
 }
