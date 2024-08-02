eb5dd2e50714b97101b7af7a60011d5bbaeffb4f
Nájala Kelly <najalakelly25@hotmail.com>
2024-08-01 21:27:16 -0300

    add pag home, cadastro, global

---
+++ frontend/quiz-app/src/components/Cadastro/index.tsx
@@ -0,0 +1,29 @@
+{/* <div class="container-form">
+
+        <img src="fotos/outer-space.png" alt="">
+
+        <div class="form">
+
+            <h1 class="titulo_form">Cadastre-se</h1>
+
+            <form action="" id="form">
+
+                <div class="input">
+                    <input required class="campo_input" type="text" id="nome">
+                    <label>Nome</label>
+                </div>
+
+                <div class="input">
+                    <input required class="campo_input" type="password" id="senha">
+                    <label>Senha</label>
+                </div>
+
+            </form>
+
+        </div>
+
+        <div class="btn">
+            <a href="/" class="btn-cadastrar">Cadastrar</a>
+        </div>
+
+    </div> */}
---
+++ frontend/quiz-app/src/components/home/index.tsx
@@ -0,0 +1,9 @@
+{/* <div class="container">
+        <div class="container-inicial">
+            <h1 class='titulo_inicial'>Teste seus conhecimentos em astronomia básica!</h1>
+            <button class="botao">Começar</button>
+        </div>
+        <div class="container-img">
+            <img src="fotos/venus.png" alt="" class="planeta">
+        </div>
+    </div>  */}
