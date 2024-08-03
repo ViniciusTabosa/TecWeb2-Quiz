7d93e30977d3ba21f4a0dfe55f4f63e86a3fe971
Vinicius Tabosa <viniciustabosa.dev@gmail.com>
2024-08-03 17:34:16 -0300

    commit

--- frontend/quiz-app/src/app/page.tsx
+++ frontend/quiz-app/src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'; // Ajuste o caminho conforme necessário
 import styles from './page.module.css'; // Se necessário, ajuste ou remova esta importação
-import { BrowserRouter } from 'react-router-dom';
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <BrowserRouter>
-      </BrowserRouter>
     </main>
   );
 }
