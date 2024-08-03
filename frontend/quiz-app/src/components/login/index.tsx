7c87f6aae580d93b852c4cc2063611dedd64ee96
Vinicius Tabosa <viniciustabosa.dev@gmail.com>
2024-08-03 17:39:03 -0300

    add login

--- frontend/quiz-app/src/components/login/index.tsx
+++ frontend/quiz-app/src/components/login/index.tsx
@@ -1,6 +1,23 @@
-export const Login = () => {
-    return(
-        <>
-        </>
-    )
-}
+{/* <div class="container-login-bg">
+        
+        <div class="container_login">
+            <img src="fotos/galaxybranca.png" alt=""> 
+            <div class="form-login">
+                <h1 class="titulo_login">Acesse seu perfil</h1>
+                <form action="" id="form2">
+                    <div class="input2">
+                        <input required class="campo_input2" type="text" id="nome2">
+                        <label class="label">Nome</label>
+                    </div>
+                    <div class="input2">
+                        <input required class="campo_input2" type="password" id="senha2">
+                        <label class="label">Senha</label>
+                    </div>
+                </form>
+            </div>
+            <div class="btn2">
+                <button class="btn-entrar">Entrar</button>
+            </div>
+        </div>
+
+    </div>    */}
