498dd26fbba9ec8e94b33d4d30fabde6fbb990f7
yarabrg <yrbraga@alu.ufc.br>
2024-08-07 18:54:21 -0300

    Ajustes na api de scores

--- backend/models/Score.js
+++ backend/models/Score.js
@@ -1,21 +1,15 @@
 import {db} from '../db.js';
 
 // funcao para criar um novo registro de pontuação
-export const criarScoreJogador = (jogador_id, pontuacao, data_criacao, callback)=>{
-    const query = 'INSERT INTO ranking (jogador_id, pontuacao, atualizado_em ) VALUES (?,?,?)'
-    db.query(query, [jogador_id, pontuacao, data_criacao], callback)
+export const criarScoreJogador = (jogador_id, pontuacao, data_criacao, tempo_conclusao, callback)=>{
+    const query = 'INSERT INTO ranking (jogador_id, pontuacao, atualizado_em, tempo_conclusao ) VALUES (?,?,?,?);'
+    db.query(query, [jogador_id, pontuacao, data_criacao, tempo_conclusao], callback)
 }
 
 // funcao para buscar o score de um jogador
 export const buscarScoresJogador = (jogador_id, callback)=>{
     const query = 'SELECT * FROM ranking WHERE jogador_id = ?';
     db.query(query, [jogador_id], callback);
-}
-
-// funcao para listar os scores de todos os jogadores
-export const buscarScoresJogadores = (callback)=>{
-    const query = 'SELECT r.id AS ranking_id, j.nome AS jogador_nome, r.jogador_id AS jogador_id, r.pontuacao AS pontuacao, r.atualizado_em AS atualizado_em FROM ranking r INNER JOIN jogadores j ON r.jogador_id = j.id ORDER BY r.id ASC;';
-    db.query(query, callback);
 }
 
 // função para excluir os scores de um jogador
@@ -26,12 +20,6 @@ export const deletarScoreJogador = (jogador_id, callback) => {
 
 // função para montar o ranking das 10 melhores pontuações
 export const rankingPontuacoes = (callback) => {
-    const query = 'SELECT ROW_NUMBER() OVER (ORDER BY rs.pontuacao DESC) AS posicao_ranking, j.nome AS jogador_nome, rs.pontuacao AS pontuacao FROM (SELECT r.jogador_id, MAX(r.pontuacao) AS pontuacao FROM ranking r GROUP BY r.jogador_id) rs INNER JOIN jogadores j ON rs.jogador_id = j.id ORDER BY rs.pontuacao DESC LIMIT 10;'
+    const query = 'SELECT ROW_NUMBER() OVER (ORDER BY rs.pontuacao DESC, rs.tempo_conclusao ASC) AS posicao_ranking, j.nome AS jogador_nome, rs.pontuacao AS pontuacao, rs.tempo_conclusao AS tempo_conclusao FROM (SELECT r.jogador_id, MAX(r.pontuacao) AS pontuacao, MIN(r.tempo_conclusao) AS tempo_conclusao FROM ranking r GROUP BY r.jogador_id) rs INNER JOIN jogadores j ON rs.jogador_id = j.id ORDER BY rs.pontuacao DESC, rs.tempo_conclusao ASC LIMIT 10;'
     db.query(query, callback);
 }
-
-
-
-
-
-
