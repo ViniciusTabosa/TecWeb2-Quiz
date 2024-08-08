9fd9f0e4e71ab2cd5828d1c75701bcf3e967c9e7
Vinicius Tabosa <viniciustabosa.dev@gmail.com>
2024-08-08 02:43:31 -0300

    quiz

--- frontend/quiz-app/src/components/Ability/index.tsx
+++ frontend/quiz-app/src/components/Ability/index.tsx
@@ -195,6 +195,241 @@
 
 // export default Ability;
 
+// "use client";
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import axios from 'axios';
+// import OxygenBar from '../OxygenBar';
+// import Quiz from '../Quiz';
+// import questions from '../../../public/data.json';
+// import './Ability.css';
+
+// interface Character {
+//   name: string;
+//   advantage: string;
+//   disadvantage: string;
+// }
+
+// interface QuestionsProps {
+//   selectedCharacter: Character | null;
+// }
+
+// const Ability: React.FC<QuestionsProps> = ({ selectedCharacter }) => {
+//   const [oxygenLevel, setOxygenLevel] = useState(100);
+//   const [isQuizOver, setIsQuizOver] = useState(false);
+//   const [penalty, setPenalty] = useState(4); // Penalidade padrão para erro
+//   const [hasUsedExclude, setHasUsedExclude] = useState(false); // Estado para rastrear se a habilidade "Excluir Alternativas Erradas" foi usada
+//   const [hasUsedDouble, setHasUsedDouble] = useState(false); // Estado para rastrear se a habilidade "Dobrar Pontos" foi usada
+//   const [hasUsedSkip, setHasUsedSkip] = useState(false); // Estado para rastrear se a habilidade "Pular Pergunta" foi usada
+//   const [excludeForCurrentQuestion, setExcludeForCurrentQuestion] = useState(false); // Estado para excluir alternativas para a pergunta atual
+//   const [doubleForCurrentQuestion, setDoubleForCurrentQuestion] = useState(false); // Estado para dobrar pontos para a pergunta atual
+//   const [infoBox, setInfoBox] = useState<string | null>(null); // Estado para controlar a exibição da caixa de informações
+//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+//   const [score, setScore] = useState(0); // Adiciona estado para a pontuação
+//   const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+//   useEffect(() => {
+//     startOxygenTimer();
+//     return () => {
+//       if (timerRef.current) {
+//         clearInterval(timerRef.current);
+//       }
+//     };
+//   }, []);
+
+//   const startOxygenTimer = () => {
+//     timerRef.current = setInterval(() => {
+//       setOxygenLevel((prevLevel) => {
+//         if (prevLevel <= 0) {
+//           clearInterval(timerRef.current!);
+//           setIsQuizOver(true);
+//           console.log('Quiz terminado: Nível de oxigênio chegou a zero.');
+//           saveScore(); // Salva a pontuação quando o quiz termina por oxigênio
+//           return 0;
+//         }
+//         const decreaseRate = 1;
+//         return Math.max(prevLevel - decreaseRate, 0);
+//       });
+//     }, 1000);
+//   };
+
+//   const saveScore = async () => {
+//     const jogador_id = JSON.parse(localStorage.getItem('usuario') || '{}').id;
+//     console.log(jogador_id);
+//     console.log(score)
+//     if (!jogador_id) return; // Verifica se o id do jogador está disponível
+
+//     try {
+//       const response = await axios.post('http://localhost:3000/score/registrarscore', {
+//         jogador_id,
+//         pontuacao: score // Pontuação obtida no quiz
+//       });
+
+//       console.log('Pontuação salva com sucesso:', response.data);
+//     } catch (error) {
+//       console.error('Erro ao salvar a pontuação:', error);
+//     }
+//   };
+
+//   const handleAnswer = (isCorrect: boolean) => {
+//     if (isQuizOver) return;
+
+//     if (isCorrect) {
+//       setOxygenLevel((prevLevel) => Math.min(prevLevel + 5, 100));
+//       setScore((prevScore) => prevScore + (doubleForCurrentQuestion ? 2 : 1)); // Atualiza a pontuação
+//       console.log('Resposta correta: Aumentou o nível de oxigênio em 5.');
+//     } else {
+//       if (selectedCharacter?.name === 'Sol the Dreamer' && hasUsedDouble) {
+//         setOxygenLevel((prevLevel) => Math.max(prevLevel - 5, 0));
+//         console.log('Resposta incorreta: Diminuiu o nível de oxigênio em 5 devido à habilidade de Sol the Dreamer.');
+//       } else {
+//         setOxygenLevel((prevLevel) => Math.max(prevLevel - penalty, 0));
+//         console.log(`Resposta incorreta: Diminuiu o nível de oxigênio em ${penalty}.`);
+//       }
+//     }
+
+//     // Resetar habilidades específicas após cada pergunta
+//     setExcludeForCurrentQuestion(false);
+//     setDoubleForCurrentQuestion(false);
+
+//     if (timerRef.current) clearInterval(timerRef.current);
+//     startOxygenTimer();
+
+//     // Verifica se todas as perguntas foram respondidas
+//     if (currentQuestionIndex === questions.length - 1) {
+//       setIsQuizOver(true);
+//       console.log('Quiz terminado: Todas as perguntas foram respondidas.');
+//       saveScore(); // Salva a pontuação quando o quiz termina por conclusão das perguntas
+//     }
+//   };
+
+//   const handleSkip = () => {
+//     if (isQuizOver || hasUsedSkip) return;
+
+//     setHasUsedSkip(true); // Marca a habilidade como usada
+//     setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length); // Avança para a próxima pergunta
+//     console.log('Pergunta pulada.');
+//   };
+
+//   const handleExcludeWrongAnswers = () => {
+//     if (isQuizOver || hasUsedExclude) return;
+
+//     setExcludeForCurrentQuestion(true); // Excluir alternativas para a pergunta atual
+//     setHasUsedExclude(true); // Marca a habilidade como usada
+//     console.log('Alternativas erradas excluídas.');
+//   };
+
+//   const handleDoublePoints = () => {
+//     if (isQuizOver || hasUsedDouble) return;
+
+//     setDoubleForCurrentQuestion(true); // Dobrar pontos para a pergunta atual
+//     setHasUsedDouble(true); // Marca a habilidade como usada
+//     setPenalty(5); // Ajusta a penalidade de oxigênio para -5
+//     console.log('Pontuação da pergunta atual dobrada.');
+//   };
+
+//   return (
+//     <div className='ability-container'>
+//       <div className="side-bar">
+//         <OxygenBar oxygenLevel={oxygenLevel} />
+//       </div>
+//       {isQuizOver ? (
+//         <div className="quiz-over">
+//           <h2>Quiz Terminado</h2>
+//           <p>Seu nível de oxigênio chegou a zero.</p>
+//         </div>
+//       ) : (
+//         <div>
+//           <Quiz 
+//             onAnswer={handleAnswer} 
+//             excludeWrongAnswers={selectedCharacter?.name === 'Astro the Navigator' && excludeForCurrentQuestion}
+//             doublePoints={selectedCharacter?.name === 'Sol the Explorer' && doubleForCurrentQuestion}
+//             currentQuestionIndex={currentQuestionIndex}
+//             setCurrentQuestionIndex={setCurrentQuestionIndex}
+//           />
+//           <div className="button-container">
+//             {selectedCharacter && selectedCharacter.name === 'Luna the Dreamer' && (
+//               <div className="button-wrapper">
+//                 <button
+//                   onClick={handleSkip}
+//                   disabled={hasUsedSkip}
+//                   onMouseEnter={() => setInfoBox('skip')}
+//                   onMouseLeave={() => setInfoBox(null)}
+//                 >
+//                   Pular Pergunta
+//                 </button>
+//                 {infoBox === 'skip' && (
+//                   <div className="info-box">
+//                     <span className="info-icon"><i className="fas fa-exclamation-circle"></i></span>
+//                     <div>
+//                       <p><strong>Vantagem:</strong> {selectedCharacter.advantage}</p>
+//                       <p><strong>Desvantagem:</strong> {selectedCharacter.disadvantage}</p>
+//                     </div>
+//                   </div>
+//                 )}
+//               </div>
+//             )}
+//             {selectedCharacter && selectedCharacter.name === 'Astro the Navigator' && (
+//               <div className="button-wrapper">
+//                 <button
+//                   onClick={handleExcludeWrongAnswers}
+//                   disabled={hasUsedExclude}
+//                   onMouseEnter={() => setInfoBox('exclude')}
+//                   onMouseLeave={() => setInfoBox(null)}
+//                 >
+//                   Excluir Alternativas Erradas
+//                 </button>
+//                 {infoBox === 'exclude' && (
+//                   <div className="info-box">
+//                     <span className="info-icon"><i className="fas fa-exclamation-circle"></i></span>
+//                     <div>
+//                       <p><strong>Vantagem:</strong> {selectedCharacter.advantage}</p>
+//                       <p><strong>Desvantagem:</strong> {selectedCharacter.disadvantage}</p>
+//                     </div>
+//                   </div>
+//                 )}
+//               </div>
+//             )}
+//             {selectedCharacter && selectedCharacter.name === 'Sol the Explorer' && (
+//               <div className="button-wrapper">
+//                 <button
+//                   onClick={handleDoublePoints}
+//                   disabled={hasUsedDouble}
+//                   onMouseEnter={() => setInfoBox('double')}
+//                   onMouseLeave={() => setInfoBox(null)}
+//                 >
+//                   Dobrar Pontos
+//                 </button>
+//                 {infoBox === 'double' && (
+//                   <div className="info-box">
+//                     <span className="info-icon"><i className="fas fa-exclamation-circle"></i></span>
+//                     <div>
+//                       <p><strong>Vantagem:</strong> {selectedCharacter.advantage}</p>
+//                       <p><strong>Desvantagem:</strong> {selectedCharacter.disadvantage}</p>
+//                     </div>
+//                   </div>
+//                 )}
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Ability;
+
+
+
+
+
+
+
+
+
+
+
 "use client";
 
 import React, { useState, useEffect, useRef } from 'react';
@@ -284,7 +519,7 @@ const Ability: React.FC<QuestionsProps> = ({ selectedCharacter }) => {
         console.log('Resposta incorreta: Diminuiu o nível de oxigênio em 5 devido à habilidade de Sol the Dreamer.');
       } else {
         setOxygenLevel((prevLevel) => Math.max(prevLevel - penalty, 0));
-        console.log(`Resposta incorreta: Diminuiu o nível de oxigênio em ${penalty}.`);
+        console.log('Resposta incorreta: Diminuiu o nível de oxigênio em ${penalty}.');
       }
     }
 
--- frontend/quiz-app/src/components/Quiz/index.tsx
+++ frontend/quiz-app/src/components/Quiz/index.tsx
@@ -1,14 +1,154 @@
-"use client"
+// "use client"
+
+// import questions from '../../../public/data.json';
+// import { useState, useEffect } from 'react';
+// import ScorePoints from '../scorepoints';
+// import './Quiz.css';
+
+// interface Alternative {
+//   id: number;
+//   texto_opcao: string;
+//   is_correta: number; // 0 para incorreto e 1 para correto
+// }
+
+// interface Question {
+//   id: number;
+//   texto_pergunta: string;
+//   pontos: number;
+//   nivel_dificuldade: number;
+//   alternativas: Alternative[];
+// }
+
+// interface QuizProps {
+//   onAnswer: (isCorrect: boolean) => void;
+//   excludeWrongAnswers?: boolean;
+//   doublePoints?: boolean;
+//   currentQuestionIndex: number;
+//   setCurrentQuestionIndex: React.Dispatch<React.SetStateAction<number>>;
+// }
+
+// const Quiz: React.FC<QuizProps> = ({ onAnswer, excludeWrongAnswers, doublePoints, currentQuestionIndex, setCurrentQuestionIndex }) => {
+//   const [score, setScore] = useState(0); // Adiciona o estado de pontuação
+//   const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null); // Para mostrar se a resposta foi correta ou não
+//   const [filteredAlternatives, setFilteredAlternatives] = useState<Alternative[]>([]);
+
+//   useEffect(() => {
+//     const currentQuestion = questions[currentQuestionIndex];
+//     setFilteredAlternatives(getFilteredAlternatives(currentQuestion));
+//   }, [currentQuestionIndex, excludeWrongAnswers]);
+
+//   const handleAnswer = (isCorrect: boolean) => {
+//     const currentQuestion = questions[currentQuestionIndex];
+//     let points = getPoints(currentQuestion.nivel_dificuldade, isCorrect);
+
+//     if (doublePoints) {
+//       points *= 2; // Dobra a pontuação se a habilidade estiver ativada
+//     }
+
+//     setScore((prevScore) => Math.max(prevScore + points, 0));
+//     setIsAnswerCorrect(isCorrect); // Armazena se a resposta foi correta ou não
+//     onAnswer(isCorrect);
+
+//     // Avança para a próxima pergunta
+//     setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
+//   };
+
+//   const getPoints = (nivelDificuldade: number, isCorrect: boolean): number => {
+//     if (excludeWrongAnswers) {
+//       switch (nivelDificuldade) {
+//         case 1:
+//           return isCorrect ? 2 : -1;
+//         case 2:
+//           return isCorrect ? -5 : -10;
+//         case 3:
+//           return isCorrect ? -5 : -15;
+//         default:
+//           return 0;
+//       }
+//     } else {
+//       switch (nivelDificuldade) {
+//         case 1:
+//           return isCorrect ? 5 : -2;
+//         case 2:
+//           return isCorrect ? 10 : -5;
+//         case 3:
+//           return isCorrect ? 15 : -7;
+//         default:
+//           return 0;
+//       }
+//     }
+//   };
+
+//   const getFilteredAlternatives = (currentQuestion: Question) => {
+//     if (excludeWrongAnswers) {
+//       const correctAlternatives = currentQuestion.alternativas.filter(alt => alt.is_correta === 1);
+//       const incorrectAlternatives = currentQuestion.alternativas.filter(alt => alt.is_correta === 0);
+
+//       // Garantir que sempre haja uma alternativa incorreta
+//       const displayedAlternatives = [...correctAlternatives];
+//       if (incorrectAlternatives.length > 0) {
+//         const randomIncorrect = incorrectAlternatives[Math.floor(Math.random() * incorrectAlternatives.length)];
+//         displayedAlternatives.push(randomIncorrect);
+//       }
+
+//       // Ordenar alternativas com base no id para manter a ordem original
+//       return displayedAlternatives.sort((a, b) => a.id - b.id);
+//     }
+//     // Ordenar alternativas com base no id para manter a ordem original
+//     return currentQuestion.alternativas.sort((a, b) => a.id - b.id);
+//   };
+
+//   const currentQuestion: Question = questions[currentQuestionIndex];
+
+//   return (
+//     <div className="quiz-container">
+//       <h2>{currentQuestion.texto_pergunta}</h2>
+//       <div className="quiz-buttons">
+//         {filteredAlternatives.map((alt) => (
+//           <button
+//             key={alt.id}
+//             className="quiz-button"
+//             onClick={() => handleAnswer(alt.is_correta === 1)}
+//           >
+//             {alt.texto_opcao}
+//           </button>
+//         ))}
+//       </div>
+//       {isAnswerCorrect !== null && (
+//         <ScorePoints
+//           nivelDificuldade={currentQuestion.nivel_dificuldade}
+//           isCorrect={isAnswerCorrect}
+//         />
+//       )}
+//       <div className="score-display">
+//         <h3>Pontuação Total: {score}</h3>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Quiz;
+
+
+
+
+
+
+
+
+
+"use client";
 
 import questions from '../../../public/data.json';
 import { useState, useEffect } from 'react';
 import ScorePoints from '../scorepoints';
 import './Quiz.css';
+import axios from 'axios'; // Importar axios para fazer requisições HTTP
 
 interface Alternative {
   id: number;
   texto_opcao: string;
-  is_correta: number; // 0 para incorreto e 1 para correto
+  is_correta: number;
 }
 
 interface Question {
@@ -28,9 +168,10 @@ interface QuizProps {
 }
 
 const Quiz: React.FC<QuizProps> = ({ onAnswer, excludeWrongAnswers, doublePoints, currentQuestionIndex, setCurrentQuestionIndex }) => {
-  const [score, setScore] = useState(0); // Adiciona o estado de pontuação
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null); // Para mostrar se a resposta foi correta ou não
+  const [score, setScore] = useState(0);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
   const [filteredAlternatives, setFilteredAlternatives] = useState<Alternative[]>([]);
+  const [quizFinished, setQuizFinished] = useState(false);
 
   useEffect(() => {
     const currentQuestion = questions[currentQuestionIndex];
@@ -42,15 +183,21 @@ const Quiz: React.FC<QuizProps> = ({ onAnswer, excludeWrongAnswers, doublePoints
     let points = getPoints(currentQuestion.nivel_dificuldade, isCorrect);
 
     if (doublePoints) {
-      points *= 2; // Dobra a pontuação se a habilidade estiver ativada
+      points *= 2;
     }
 
     setScore((prevScore) => Math.max(prevScore + points, 0));
-    setIsAnswerCorrect(isCorrect); // Armazena se a resposta foi correta ou não
+    setIsAnswerCorrect(isCorrect);
     onAnswer(isCorrect);
 
-    // Avança para a próxima pergunta
-    setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
+    // Avança para a próxima pergunta ou termina o quiz
+    const nextIndex = (currentQuestionIndex + 1) % questions.length;
+    if (nextIndex === 0) {
+      // Assumindo que o quiz termina após a última pergunta
+      setQuizFinished(true);
+    } else {
+      setCurrentQuestionIndex(nextIndex);
+    }
   };
 
   const getPoints = (nivelDificuldade: number, isCorrect: boolean): number => {
@@ -84,19 +231,37 @@ const Quiz: React.FC<QuizProps> = ({ onAnswer, excludeWrongAnswers, doublePoints
       const correctAlternatives = currentQuestion.alternativas.filter(alt => alt.is_correta === 1);
       const incorrectAlternatives = currentQuestion.alternativas.filter(alt => alt.is_correta === 0);
 
-      // Garantir que sempre haja uma alternativa incorreta
       const displayedAlternatives = [...correctAlternatives];
       if (incorrectAlternatives.length > 0) {
         const randomIncorrect = incorrectAlternatives[Math.floor(Math.random() * incorrectAlternatives.length)];
         displayedAlternatives.push(randomIncorrect);
       }
 
-      // Ordenar alternativas com base no id para manter a ordem original
       return displayedAlternatives.sort((a, b) => a.id - b.id);
     }
-    // Ordenar alternativas com base no id para manter a ordem original
     return currentQuestion.alternativas.sort((a, b) => a.id - b.id);
   };
+
+  const saveScore = async () => {
+    const jogador_id = JSON.parse(localStorage.getItem('usuario') || '{}').id;
+    if (!jogador_id) return; // Verifica se o id do jogador está disponível
+
+    try {
+      await axios.post('http://localhost:3000/score/registrarscore', {
+        jogador_id,
+        pontuacao: score
+      });
+      console.log('Pontuação salva com sucesso!');
+    } catch (error) {
+      console.error('Erro ao salvar a pontuação:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (quizFinished) {
+      saveScore();
+    }
+  }, [quizFinished]);
 
   const currentQuestion: Question = questions[currentQuestionIndex];
 
@@ -128,6 +293,48 @@ const Quiz: React.FC<QuizProps> = ({ onAnswer, excludeWrongAnswers, doublePoints
 };
 
 export default Quiz;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 'use client'
 
